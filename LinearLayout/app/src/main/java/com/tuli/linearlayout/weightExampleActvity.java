860e0207be975dc2cffad31138937cc78b521579
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-04-05 14:05:44 +0600

    Linear Layout

---
+++ LinearLayout/app/src/main/java/com/tuli/linearlayout/weightExampleActvity.java
@@ -0,0 +1,14 @@
+package com.tuli.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weightExampleActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_example_actvity);
+    }
+}
